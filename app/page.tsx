6e34fc25b54050d8dac200f691a6e1366d9345ac
elinoza <hillcakmak@gmail.com>
2025-03-02 10:56:00 +0300

    page names changed

--- app/page.tsx
+++ app/page.tsx
@@ -4,82 +4,146 @@ export default function Home() {
       <main className="min-h-screen w-full flex flex-col items-center justify-center relative main-card container">
         <div className="flex items-center justify-center flex-wrap gap-6 mt-8">
           <a
-            href="https://edu-games-three.vercel.app/atomTeorileri"
+            href="https://edu-games-three.vercel.app/911"
             className="px-6 py-3 bg-gradient-to-r from-purple-600 to-blue-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-purple-700 hover:to-blue-600 transition-all duration-300 ease-in-out"
           >
-            Atom Teorileri
+            Kimya biliminin günlük hayata katkısına ilişkin çıkarım yapabilme
+            (9.1.1)
           </a>
           <a
-            href="https://edu-games-three.vercel.app/bilesik"
-            className="px-6 py-3 bg-gradient-to-r from-green-600 to-teal-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-green-700 hover:to-teal-600 transition-all duration-300 ease-in-out"
-          >
-            Bileşik
-          </a>
-          <a
-            href="https://edu-games-three.vercel.app/elektron"
-            className="px-6 py-3 bg-gradient-to-r from-pink-600 to-red-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-pink-700 hover:to-red-600 transition-all duration-300 ease-in-out"
-          >
-            Elektron
-          </a>
-          <a
-            href="https://edu-games-three.vercel.app/iyon"
-            className="px-6 py-3 bg-gradient-to-r from-yellow-600 to-orange-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-yellow-700 hover:to-orange-600 transition-all duration-300 ease-in-out"
-          >
-            İyon
-          </a>
-          <a
-            href="https://edu-games-three.vercel.app/orbitaller"
-            className="px-6 py-3 bg-gradient-to-r from-indigo-600 to-purple-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-indigo-700 hover:to-purple-600 transition-all duration-300 ease-in-out"
-          >
-            Orbitaller
-          </a>
-          <a
-            href="https://edu-games-three.vercel.app/piktogram"
+            href="https://edu-games-three.vercel.app/912"
             className="px-6 py-3 bg-gradient-to-r from-blue-600 to-cyan-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
           >
-            Piktogram
+            Piktogram (9.1.2)
           </a>
           <a
-            href="https://edu-games-three.vercel.app/periyodikTablo"
+            href="https://edu-games-three.vercel.app/913"
+            className="px-6 py-3 bg-gradient-to-r from-purple-600 to-blue-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-purple-700 hover:to-blue-600 transition-all duration-300 ease-in-out"
+          >
+            Atom Teorileri sıralama oyunu (9.1.3)
+          </a>
+          <a
+            href="https://edu-games-three.vercel.app/914a"
+            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+          >
+            Orbital Tanımları(9.1.4.a)
+          </a>
+          <a
+            href="https://edu-games-three.vercel.app/914b"
+            className="px-6 py-3 bg-gradient-to-r from-indigo-600 to-purple-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-indigo-700 hover:to-purple-600 transition-all duration-300 ease-in-out"
+          >
+            Orbitaller oyunu(914b)
+          </a>
+          <a
+            href=""
+            className="px-6 py-3 bg-gradient-to-r from-indigo-600 to-purple-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-indigo-700 hover:to-purple-600 transition-all duration-300 ease-in-out"
+          >
+            Orbitaller oyunu(9.1.5) YOK?
+          </a>
+          <a
+            href="https://edu-games-three.vercel.app/916a"
             className="px-6 py-3 bg-gradient-to-r from-sky-600 to-emerald-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
           >
-            Periyodik Tablo
+            Periyodik Tablo (9.1.6.a)
+          </a>
+
+          <a
+            href="https://edu-games-three.vercel.app/916b"
+            className="px-6 py-3 bg-gradient-to-r from-pink-600 to-red-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-pink-700 hover:to-red-600 transition-all duration-300 ease-in-out"
+          >
+            Elektron (9.1.6b) Farklı elementlerin elektron dizilimlerine göre
+            doğru periyot ve gruplara sürükle bırak yöntemiyle yerleştirildiği
+            bir etkileşimli içerik hazırlanır.
           </a>
           <a
-            href="https://edu-games-three.vercel.app/orbitalTanim"
+            href="https://edu-games-three.vercel.app/917"
             className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
           >
-            Orbital Tanımları
+            İyon oluşumuna ilişkin tümevarımsal akıl yürütebilme(9.1.7)
+          </a>
+
+          <a
+            href="https://edu-games-three.vercel.app/918a"
+            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+          >
+            A. Atom numaraları verilen farklı atomların yarıçaplarını ve
+            iyonlaşma enerjilerini periyodik tablodaki konumları ile B.
+            ilişkilendirerek sıralanmasını sağlayan etkileşimli içerik
+            hazırlanır.(9.1.8a)
           </a>
           <a
-            href="https://edu-games-three.vercel.app/benzetim"
+            href=""
             className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
           >
-            Kaynama noktası-Basınç
+            Atom numaraları verilen farklı atomların elektronegatiflik
+            değerlerinin periyodik tablodaki konumu ile ilişkilendirilerek
+            sıralanmasını sağlayan etkileşimli içerik hazırlanır.(yukarıdaki
+            gibi benzer etkinlik)(9.1.8b) YOK?
           </a>
           <a
-            href="https://edu-games-three.vercel.app/basinc"
-            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+            href="https://edu-games-three.vercel.app/922"
+            className="px-6 py-3 bg-gradient-to-r from-yellow-600 to-orange-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-yellow-700 hover:to-orange-600 transition-all duration-300 ease-in-out"
           >
-            Buhar Basıncı
+            İyonik bağın oluşumunu bilimsel gözleme dayalı tahmin
+            edebilme(9.2.2)
           </a>
           <a
-            href="https://edu-games-three.vercel.app/viskozite"
-            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+            href=""
+            className="px-6 py-3 bg-gradient-to-r from-yellow-600 to-orange-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-yellow-700 hover:to-orange-600 transition-all duration-300 ease-in-out"
           >
-            Viskozite
+            Farklı elementlerin atomlarının seçilerek, valans elektronlarının
+            doğru şekilde yerleştirilerek moleküllerin Lewis yapılarının
+            oluşturulduğu bir oyun hazırlanır. (9.2.4) YOK??
           </a>
           <a
-            href="https://edu-games-three.vercel.app/viskozite"
-            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+            href=""
+            className="px-6 py-3 bg-gradient-to-r from-yellow-600 to-orange-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-yellow-700 hover:to-orange-600 transition-all duration-300 ease-in-out"
           >
-            sıvı ve yüzey (hazırlanıyor)
+            Verilen farklı element ve bileşiklerinin polar yapılı ya da apolar
+            yapılı şeklinde sınıflandırmalarına olanak sağlayan etkileşimli
+            içerik hazırlanır. (9.2.5) YOK??
           </a>
           <a
-            href="https://edu-games-three.vercel.app/dizilim"
+            href="https://edu-games-three.vercel.app/926"
+            className="px-6 py-3 bg-gradient-to-r from-green-600 to-teal-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-green-700 hover:to-teal-600 transition-all duration-300 ease-in-out"
+          >
+            9.2.6
+          </a>
+          <a
+            href="https://edu-games-three.vercel.app/929"
             className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
           >
-            9.1.7
+            Buhar Basıncı (9.2.9)
+          </a>
+
+          <a
+            href="https://edu-games-three.vercel.app/9.2.10"
+            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+          >
+            Kaynama noktası-Basınç (9.2.10)
+          </a>
+
+          <a
+            href="https://edu-games-three.vercel.app/9.2.11"
+            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+          >
+            Viskozite (9.2.11)
+          </a>
+          <a
+            href="9.2.12"
+            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+          >
+            Öğrencilerin farklı sıvıları ve yüzeyleri seçerek yüzey gerilimi,
+            kılcallık, içbükey-dışbükey görünümü ve yüzeyi ıslatma/ıslatmama
+            özelliklerinin gözlemlenebileceği bir benzetim / simülasyon
+            hazırlanır (9.2.12) YOK
+          </a>
+          <a
+            href="https://edu-games-three.vercel.app/931"
+            className="px-6 py-3 bg-gradient-to-r from-emerald-600 to-green-500 text-white font-semibold text-lg rounded-lg shadow-lg transform hover:scale-105 hover:from-blue-700 hover:to-cyan-600 transition-all duration-300 ease-in-out"
+          >
+            Evsel atıklardan metal nanoparçacık elde etmek üzere deney
+            yapabilme(9.3.1) YOK
           </a>
         </div>
       </main>
